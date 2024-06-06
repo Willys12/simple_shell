ac48ed830b3de6ea83d6abe65aad7c65234309ea
Willys12 <ben.willys92@gmail.com>
2024-06-06 13:23:44 +0300

    a

--- _setenv.c
+++ _setenv.c
@@ -1,47 +1,24 @@
 #include "builtins.h"
 
 /**
- * __setenv - sets the environment variables
- * @info: arguments passed
- *
- * Return: status
- */
-int __setenv(info_t *info)
+  * __unsetenv - unsets the environment variable
+  * @info: arguments passed
+  * Return: status
+  */
+int __unsetenv(info_t *info)
 {
-	env_t *var;
-	char **args = info->tokens + 1, *val;
+	char **args = info->tokens + 1;
 
-	if (args[0])
+	if (*args)
 	{
-		if (args[1])
-		{
-			if (args[2])
-			{
-				perrorl("Too many arguments.", *info->tokens, NULL);
-				info->status = EXIT_FAILURE;
-				return (info->status);
-			}
-			val = args[1];
-		}
-		else
-		{
-			val = "";
-		}
-		var = get_dict_node(info->env, args[0]);
-		if (var)
-		{
-			free(var->val);
-			var->val = _strdup(val);
-		}
-		else
-		{
-			add_dict_node_end(&info->env, args[0], val);
-		}
+		while (*args)
+			del_dict_node(&info->env, *args++);
 		info->status = EXIT_SUCCESS;
 	}
 	else
 	{
-		__env(info);
+		perrorl("Too few arguments.", *info->tokens, NULL);
+		info->status = EXIT_FAILURE;
 	}
 	return (info->status);
 }
